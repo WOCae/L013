879155cad954d8b10bf91b247579c266816843e5
WOCae <t-satoIZ@outlook.jp>
2024-02-15 13:16:04 +0900

    Update main.gd

--- main.gd
+++ main.gd
@@ -10,6 +10,8 @@ func _ready():
 	
 	pass
 
+var map2Flag:bool
+
 func _process(delta):
 	
 	var cnt = 0 #MAP2のカウント
@@ -19,9 +21,27 @@ func _process(delta):
 		#print(child.name)
 		if "MAP2" in child.name:
 			cnt += 1
+			map2Flag = true
 			#print("MAP2" + str(cnt))
 	print("個数：" + str(cnt))
 	if cnt == 0:
 		#print("NoMAP2" )
+		map2Flag = false
 		pass
 	#----------------------
+
+	if map2Flag == false:
+		var map2 = map2.instantiate()
+		map2.position.x = randum(0,800) #出現位置を
+		self.add_child(map2)
+
+
+func randum(min_value,max_value):
+	#var random_number = randi(10)
+	#print(random_number) # 例：5
+
+	#var min_value = 0
+	#var max_value = 800
+	var random_number = randi_range(min_value, max_value)
+	#print(random_number) # 例：12
+	return random_number

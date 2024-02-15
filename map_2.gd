a09d13bab758812e585dc6b806e0a4c14d57c2c8
WOCae <t-satoIZ@outlook.jp>
2024-02-15 12:16:23 +0900

    Update map_2.gd

--- map_2.gd
+++ map_2.gd
@@ -6,3 +6,8 @@ func _process(delta):
 	velocity.y = 100
 
 	position += velocity * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	pass # Replace with function body.

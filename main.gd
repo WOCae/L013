610d2331744285a7555df795c95d51dea38bd7ae
WOCae <t-satoIZ@outlook.jp>
2024-02-15 12:30:58 +0900

    Update main.gd

--- main.gd
+++ main.gd
@@ -9,3 +9,19 @@ func _ready():
 	
 	
 	pass
+
+func _process(delta):
+	
+	var cnt = 0 #MAP2のカウント
+	
+	#-----ツリーのノード名を検索
+	for child in get_children():
+		#print(child.name)
+		if "MAP2" in child.name:
+			cnt += 1
+			#print("MAP2" + str(cnt))
+	print("個数：" + str(cnt))
+	if cnt == 0:
+		#print("NoMAP2" )
+		pass
+	#----------------------

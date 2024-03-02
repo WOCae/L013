9862ab1ec593ee3239969bd4c519430024d4098f
WOCae <t-satoIZ@outlook.jp>
2024-03-02 21:41:47 +0900

    Create main.gd

---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func _ready():
+	$Camera2D.position = Vector2(0,0)
+
+var Dpos:float = 1000
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO	
+	if Input.is_action_pressed("ui_left"):
+		$Camera2D.position.x -= Dpos*delta
+		
+	if Input.is_action_pressed("ui_right"):
+		$Camera2D.position.x += Dpos*delta
+	
+	if Input.is_action_pressed("ui_up"):
+		$Camera2D.position.y -= Dpos*delta
+		
+	if Input.is_action_pressed("ui_down"):
+		$Camera2D.position.y += Dpos*delta
